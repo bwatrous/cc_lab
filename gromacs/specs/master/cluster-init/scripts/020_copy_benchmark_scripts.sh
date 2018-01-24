4c77c29949ad9810ab7b4ab2a2ef03bc4474ef79
Ben Watrous <ben.watrous@microsoft.com>
2018-01-23 18:56:24 -0800

    fix home directory expansion

--- gromacs/specs/master/cluster-init/scripts/020_copy_benchmark_scripts.sh
+++ gromacs/specs/master/cluster-init/scripts/020_copy_benchmark_scripts.sh
@@ -6,9 +6,9 @@ export GROMACS_HOME=/opt/gromacs-5.1.4
 set -x
 set -e
 
-cp ${CYCLECLOUD_SPEC_PATH}/files/run_benchmarks_mpi.sh ~${CLUSTER_USER}/
-cp ${CYCLECLOUD_SPEC_PATH}/files/gromacs_mpi.sub ~${CLUSTER_USER}/
+cp ${CYCLECLOUD_SPEC_PATH}/files/run_benchmarks_mpi.sh /shared/home/${CLUSTER_USER}/
+cp ${CYCLECLOUD_SPEC_PATH}/files/gromacs_mpi.sub /shared/home/${CLUSTER_USER}/
 
 chown -R ${CLUSTER_USER}:${CLUSTER_USER} /shared/home/${CLUSTER_USER}/
-chmod 755 ~${CLUSTER_USER}/*.sh
+chmod 755 /shared/home/${CLUSTER_USER}/*.sh
 
